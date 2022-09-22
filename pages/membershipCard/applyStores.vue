71162a7d32d4eedaba69c271f21517432fd4f374
dreamisfarfly <dreamisfarfly@gmail.com>
2022-09-22 15:20:58 +0800

    main

---
+++ pages/membershipCard/applyStores.vue
@@ -0,0 +1,74 @@
+<template>
+  <!-- start 适用门店 -->
+  <view>
+    <!-- start 导航栏 -->
+    <u-navbar
+      :is-back="false"
+      title="（代金券10元）适用门店"
+      :background="background"
+      title-color="#333333"
+      title-size="32"
+      :title-bold="true"
+      :title-width="400"
+    >
+    </u-navbar>
+    <!-- end 导航栏 -->
+    <!-- start store-list-content -->
+    <view class="store-list-content">
+      <view style="height: 100vh">
+        <Classify :classifyList="classifyList">
+          <template v-slot:content>
+            <ShopCard v-for="(item, key) in 10" :key="key"></ShopCard>
+          </template>
+        </Classify>
+      </view>
+    </view>
+    <!-- end store-list-content -->
+  </view>
+  <!-- end 适用门店 -->
+</template>
+
+<script>
+import Classify from "@/components/classify/classify.vue";
+import ShopCard from "@/components/shopCard/shopCard.vue";
+export default {
+  components: { Classify, ShopCard },
+  data() {
+    return {
+      // 导航栏背景
+      background: {
+        backgroundColor: "#FFFFFF",
+      },
+      // 分类列表
+      classifyList: [
+        {
+          id: 0,
+          name: "全部",
+        },
+        {
+          id: 1,
+          name: "餐饮",
+        },
+        {
+          id: 2,
+          name: "住宿",
+        },
+        {
+          id: 3,
+          name: "生活",
+        },
+        {
+          id: 4,
+          name: "医疗医疗"
+        },
+        {
+          id: 7,
+          name: "xx",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="less" scoped></style>
