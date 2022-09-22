03ed864ad89de4079948c64a67dc1431258d8c21
dreamisfarfly <dreamisfarfly@gmail.com>
2022-09-22 15:55:04 +0800

    main

--- pages/storeList/storeList.vue
+++ pages/storeList/storeList.vue
@@ -12,10 +12,71 @@
     </u-navbar>
     <!-- start store-list-content -->
     <view class="store-list-content">
-      <view style="height:100vh;">
+      <!-- start classify-vessel -->
+      <view class="classify-vessel">
+        <!-- start classify-list -->
+        <view class="classify-list">
+          <view class="classify-info">
+            <img
+              src="/static/images/classify-site-icon.png"
+              class="classify-icon"
+            />
+            <view class="classify-content">距离</view>
+          </view>
+          <img src="/static/images/not-bottom-icon.png" class="reverse-icon" />
+        </view>
+        <!-- end classify-list -->
+        <!-- start separator -->
+        <view class="separator"></view>
+        <!-- end separator -->
+        <!-- start classify-list -->
+        <view class="classify-list">
+          <view class="classify-info">
+            <img
+              src="/static/images/classify-site-icon.png"
+              class="classify-icon"
+            />
+            <view class="classify-content">所在地</view>
+          </view>
+          <img src="/static/images/not-bottom-icon.png" class="reverse-icon" />
+        </view>
+        <!-- end classify-list -->
+        <!-- start separator -->
+        <view class="separator"></view>
+        <!-- end separator -->
+        <!-- start classify-list -->
+        <view class="classify-list">
+          <view class="classify-info">
+            <img
+              src="/static/images/classify-evaluate-icon.png"
+              class="classify-icon"
+            />
+            <view class="classify-content">评价</view>
+          </view>
+          <img src="/static/images/not-bottom-icon.png" class="reverse-icon" />
+        </view>
+        <!-- end classify-list -->
+        <!-- start separator -->
+        <view class="separator"></view>
+        <!-- end separator -->
+        <!-- start classify-list -->
+        <view class="classify-list">
+          <view class="classify-info">
+            <img
+              src="/static/images/classify-discount-icon.png"
+              class="classify-icon"
+            />
+            <view class="classify-content">折扣</view>
+          </view>
+          <img src="/static/images/not-bottom-icon.png" class="reverse-icon" />
+        </view>
+        <!-- end classify-list -->
+      </view>
+      <!-- end classify-vessel -->
+      <view style="height: 100vh">
         <Classify :classifyList="classifyList">
           <template v-slot:content>
-            <ShopCard v-for="(item,key) in 10" :key="key"></ShopCard>
+            <ShopCard v-for="(item, key) in 10" :key="key"></ShopCard>
           </template>
         </Classify>
       </view>
@@ -37,8 +98,8 @@
 
 <script>
 import Search from "@/components/search/search.vue";
-import Classify from "../../components/classify/classify.vue";
-import ShopCard from "../../components/shopCard/shopCard.vue";
+import Classify from "@/components/classify/classify.vue";
+import ShopCard from "@/components/shopCard/shopCard.vue";
 export default {
   data() {
     return {
@@ -169,5 +230,45 @@ export default {
   padding: 8rpx 30rpx;
   background: #ffffff;
   box-sizing: border-box;
+}
+
+.classify-vessel {
+  display: flex;
+  width: 100%;
+  background: #f3f3f3;
+  align-items: center;
+  .separator {
+    width: 2rpx;
+    height: 34rpx;
+    opacity: 0.05;
+    background: #000000;
+  }
+  .classify-list {
+    flex: 1;
+    padding: 34rpx 20rpx 26rpx 16rpx;
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    .classify-info {
+      flex: 1;
+      display: flex;
+      align-items: center;
+      .classify-content {
+        flex: 1;
+        font-size: 28rpx;
+        font-weight: 400;
+        color: #333333;
+      }
+      .classify-icon {
+        width: 32rpx;
+        height: 32rpx;
+        margin-right: 4rpx;
+      }
+    }
+    .reverse-icon {
+      width: 14rpx;
+      height: 10rpx;
+    }
+  }
 }
 </style>
